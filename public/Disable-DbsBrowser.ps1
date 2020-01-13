44ce8663e1f9ab46b6a1002c31b5f0ea48822169
Tracy Boggiano <Tracy.Boggiano@docusign.com>
2020-01-13 09:28:44 -0500

    intial commit

---
+++ public/Disable-DbsBrowser.ps1
@@ -0,0 +1,54 @@
+function Disable-DbsBrowser {
+    <#
+        .SYNOPSIS
+            Disable and stop broswer service
+
+        .DESCRIPTION
+            Disable and stop broswer service on computers with no named instances
+
+        .PARAMETER ComputerName
+            The SQL Server (or server in general) that you're connecting to. This command handles named instances.
+
+        .PARAMETER Credential
+            Credential object used to connect to the computer as a different user.
+
+        .PARAMETER EnableException
+            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+        .NOTES
+            Tags: DISA, STIG
+            Author: Tracy Boggiano (@TracyBoggiano), databasesuperhero.com
+            Copyright: (c) 2010 by Chrissy LeMaire, licensed under MIT
+            License: MIT https://opensource.org/licenses/MITl
+
+        .EXAMPLE
+            PS C:\> Disable-DbsBrowser -ComputerName Sql2016
+            Disables and stops Browser service is not named instances exist
+
+        .LINK
+        https://dbadisa.readthedocs.io/en/latest/functions/Disable-DbsBrowser/
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Medium")]
+    param (
+        [parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [string[]]$ComputerName,
+        [PSCredential]$Credential,
+        [switch]$EnableException
+    )
+
+    process {
+        foreach ($Computer in $ComputerName) {
+            $instances = Get-DbaService -ComputerName $Computer -Type "Engine" -Credential $Crendential
+
+            $instanceNames = $instances | Where-Object InstanceName -ne "MSSQLSERVER"
+
+            if (@($instanceNames).Count -eq 0) {
+                Stop-DbaService -ComputerName $Computer -Type "Browser"
+                Set-Service -ComputerName $Computer -DisplayName "Browser" -StartupType "Disabled"
+            }
+        }
+    }
+}
