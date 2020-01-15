0d05ae12783fade918c23dc5743dc12b5c977a47
Chrissy LeMaire <clemaire@gmail.com>
2020-01-15 22:06:54 +0100

    big ol update

--- public/Test-DbsInstallPath.ps1
+++ public/Test-DbsInstallPath.ps1
@@ -1,20 +1,17 @@
 function Test-DbsInstallPath {
     <#
     .SYNOPSIS
-        Returns a list of any server that has mixed mode enabled
+        Tests the installation path to ensure it is not part of the Windows directory or in an application folder
 
     .DESCRIPTION
-        Returns a list of any server that has mixed mode enabled
+        Tests the installation path to ensure it is not part of the Windows directory or in an application folder
 
-    .PARAMETER SqlInstance
-        The target SQL Server instance or instances.
+    .PARAMETER ComputerName
+        The target SQL Server
 
-    .PARAMETER SqlCredential
-        Login to the target instance using alternative credentials. Accepts PowerShell credentials (Get-Credential).
+    .PARAMETER Credential
+        Credential object used to connect to the computer as a different user
 
-        Windows Authentication, SQL Server Authentication, Active Directory - Password, and Active Directory - Integrated are all supported.
-
-        For MFA support, please use Connect-DbaInstance.
 
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
@@ -22,30 +19,54 @@ function Test-DbsInstallPath {
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
     .NOTES
-        Tags: DISA, STIG
+        Tags: DISA, STIG, V-79169
         Author: Chrissy LeMaire (@cl), netnerds.net
 
         Copyright: (c) 2020 by Chrissy LeMaire, licensed under MIT
         License: MIT https://opensource.org/licenses/MIT
 
     .EXAMPLE
-        PS C:\> Test-DbsInstallPath -SqlInstance sql2017, sql2016, sql2012
+        PS C:\> Test-DbsInstallPath -ComputerName sql01
 
-        Returns a list of any server that has mixed mode enabled
+        Tests the installation path for all instances on sql01 to ensure they are not part of the Windows directory or in an application folder
+
     #>
-
     [CmdletBinding()]
     param (
-        [parameter(Mandatory, ValueFromPipeline)]
-        [DbaInstanceParameter[]]$SqlInstance,
-        [PsCredential]$SqlCredential,
+        [parameter(ValueFromPipeline)]
+        [Alias("cn", "host", "Server")]
+        [DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
+        [PSCredential]$Credential,
         [switch]$EnableException
     )
     process {
-        $servers = Connect-DbaInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential -DisableException:$($EnableException -eq $false)
-        foreach ($server in $servers) {
-            $root = $server.RootDirectory
-            write-warning $root
+        foreach ($computer in $ComputerName.ComputerName) {
+            $regroots = Get-DbaRegistryRoot -Computer $computer -Credential $Credential | Select-Object -ExpandProperty RegistryRoot
+            foreach ($regroot in $regroots) {
+                Invoke-PSFCommand -ComputerName $computer -Credential $Credential -ArgumentList "$regroot\Setup" -ScriptBlock {
+                    $rootdir = $currentdir = Get-ItemProperty -Path $args | Select-Object -ExpandProperty SqlPath
+                    $osroot = ([system.io.directoryinfo]$rootdir).Root
+                    $appdirmatch = $false
+                    do {
+                        $currentdir = Split-Path -Path $currentdir
+                        if ($currentdir -ne $osroot) {
+                            $appmatch = Get-ChildItem -Path "$currentdir\*exe"
+                            if ($appmatch) {
+                                $appdirmatch = $true
+                            }
+                        }
+                    } until ( ($currentdir -eq $osroot) -or $appmatch)
+
+                    [pscustomobject]@{
+                        ComputerName = $env:COMPUTERNAME
+                        RootDir      = $rootdir
+                        WinDir       = $env:windir
+                        WindirMatch  = $env:windir -match [regex]::escape($rootdir)
+                        AppdirMatch  = $appdirmatch
+                        AppdirExe    = $appmatch
+                    }
+                } | Select-Object -Property * -ExcludeProperty PSComputerName, RunspaceId
+            }
         }
     }
 }
