93539ed28b8a49a370013446a5f265167da40b4c
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2020-01-18 15:05:08 +0100

    Fips commands initial commit (#32)

---
+++ public/Get-DbsFips.ps1
@@ -0,0 +1,56 @@
+function Get-DbsFips {
+    <#
+    .SYNOPSIS
+        Gets FIPS disabled state
+
+    .DESCRIPTION
+        Gets FIPS disabled state
+
+    .PARAMETER ComputerName
+        The SQL Server (or server in general) that you're connecting to.
+
+    .PARAMETER Credential
+        Credential object used to connect to the computer as a different user.
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically Gets advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+        Tags: DISA, STIG
+        AAuthor: Chrissy LeMaire (@cl), netnerds.net
+        Copyright: (c) 2020 by Chrissy LeMaire, licensed under MIT
+        License: MIT https://opensource.org/licenses/MITl
+
+    .LINK
+        https://dbadisa.readthedocs.io/en/latest/functions/Get-DbsFips
+
+    .EXAMPLE
+        PS C:\> Get-DbsFips -ComputerName sql2016, sql2017, sql2012
+        Gets FIPS disabled state from sql2016, sql2017 and sql2012
+#>
+
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipeline)]
+        [string[]]$ComputerName,
+        [PSCredential]$Credential,
+        [switch]$EnableException
+    )
+    process {
+        foreach ($computer in $ComputerName) {
+            try {
+                $enabled = Invoke-Command2 -ComputerName $computer -Credential $credential -ScriptBlock {
+                    Get-ItemProperty HKLM:\System\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy | Select-Object -ExpandProperty Enabled
+                } -Raw
+                [pscustomobject]@{
+                    ComputerName = $computer
+                    FipsDisabled = $($enabled -eq $false)
+                }
+            } catch {
+                Stop-Function -Message "Failure" -ErrorRecord $_ -Continue -EnableException:$EnableException
+            }
+        }
+    }
+}
