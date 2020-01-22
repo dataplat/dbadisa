ba4ca6bb988d335349037d62332d7443be4d8607
Chrissy LeMaire <clemaire@gmail.com>
2020-01-22 13:34:45 +0100

    Get-DbsAuditSchemaAccessGroup initial commit

---
+++ public/Get-DbsAuditSchemaAccessGroup.ps1
@@ -0,0 +1,60 @@
+function Get-DbsAuditSchemaAccessGroup {
+    <#
+    .SYNOPSIS
+        Gets a list of audits that record when privileges/permissions are retrieved.
+
+    .DESCRIPTION
+        Gets a list of audits that record when privileges/permissions are retrieved.
+
+    .PARAMETER SqlInstance
+        The target SQL Server instance or instances. Server version must be SQL Server version 2012 or higher.
+
+    .PARAMETER SqlCredential
+        Login to the target instance using alternative credentials. Accepts PowerShell credentials (Get-Credential).
+
+        Windows Authentication, SQL Server Authentication, Active Directory - Password, and Active Directory - Integrated are all supported.
+
+        For MFA support, please use Connect-DbaInstance.
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+        Tags: V-79137
+        Author: Chrissy LeMaire (@cl), netnerds.net
+
+        Copyright: (c) 2020 by Chrissy LeMaire, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .EXAMPLE
+        PS C:\> Get-DbsAuditSchemaAccessGroup -SqlInstance sql2017, sql2016, sql2012
+
+        Gets a list of audits that record when privileges/permissions are retrieved for sql2017, sql2016 and sql2012
+
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipeline)]
+        [DbaInstanceParameter[]]$SqlInstance,
+        [PsCredential]$SqlCredential,
+        [switch]$EnableException
+    )
+    process {
+        $server = Connect-DbaInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential
+
+        try {
+            $server.Query("SELECT @@SERVERNAME as SqlInstance, a.name AS 'AuditName',
+            s.name AS 'SpecName',
+            d.audit_action_name AS 'ActionName',
+            d.audited_result AS 'Result'
+            FROM sys.server_audit_specifications s
+            JOIN sys.server_audits a ON s.audit_guid = a.audit_guid
+            JOIN sys.server_audit_specification_details d ON s.server_specification_id = d.server_specification_id
+            WHERE a.is_state_enabled = 1 AND d.audit_action_name = 'SCHEMA_OBJECT_ACCESS_GROUP'")
+        } catch {
+            Stop-Function -Message "Failure for $($server.Name)" -ErrorRecord $_ -Continue -EnableException:$EnableException
+        }
+    }
+}
