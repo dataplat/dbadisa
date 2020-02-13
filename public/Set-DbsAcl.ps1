d24f109660a1e07370bb85121a55152b202fe75c
Chrissy LeMaire <clemaire@gmail.com>
2020-02-13 23:12:28 +0100

    still broken - try changing the owner earlier

--- public/Set-DbsAcl.ps1
+++ public/Set-DbsAcl.ps1
@@ -51,14 +51,16 @@ function Set-DbsAcl {
         License: MIT https://opensource.org/licenses/MIT
 
     .EXAMPLE
-        PS C:\> Set-DbsAcl -SqlInstance sql2017, sql2016, sql2012 -Account "AD\SQL Admins"
+        PS C:\> Set-DbsAcl -SqlInstance sql2017, sql2016, sql2012 -Account "AD\SQL Admins" -Owner "AD\SQL Service"
 
         Sets permissions for the default data, log and backups on sql2017, sql2016, sql2012.
 
-        Adds appropriate permissions for the "AD\SQL Admins" group as well as the SQL Server service accounts.
+        Adds appropriate permissions for the "AD\SQL Admins" group as well as the SQL Server service accountsas Full Access.
+
+        Also sets the owner of the folder to "AD\SQL Service"
 
     .EXAMPLE
-        PS C:\> Get-DbaRegServer -SqlInstance sqlcentral | Set-DbsAcl -Account "AD\SQL Admins"
+        PS C:\> Get-DbaRegServer -SqlInstance sqlcentral | Set-DbsAcl -Account "AD\SQL Admins" -Owner "AD\SQL Service"
 
         Sets the appropriate permissions for all SQL Servers stored in the sqlcentral registered server.
     #>
@@ -69,10 +71,15 @@ function Set-DbsAcl {
         [PsCredential]$SqlCredential,
         [parameter(Mandatory)]
         [string[]]$Account,
+        [parameter(Mandatory)]
+        [string]$Owner,
         [string[]]$Path,
         [switch]$EnableException
     )
 
+    begin {
+        $PSDefaultParameterValues['*:WarningAction'] = "SilentlyContinue"
+    }
     process {
         foreach ($instance in $SqlInstance) {
             try {
@@ -86,48 +93,54 @@ function Set-DbsAcl {
                 $Path = $defaults.Data, $defaults.Log, $defaults.Backup | Where-Object { $_ -notmatch '\\\\' } | Select-Object -Unique
             }
 
-            if ($PSCmdlet.ShouldProcess($instance, "Changing permissions for $Path for $instance")) {
-                try {
-                    $instancename = $instance.InstanceName
-                    $services = Get-DbaService -ComputerName $instance
-                    $dbengine = $services | Where-Object DisplayName -match "SQL Server \($instancename\)"
-                    $dbaccount = $dbengine.StartName
-                    $agentengine = $services | Where-Object DisplayName -match "SQL Server Agent \($instancename\)"
-                    $agentaccount = $agentengine.StartName
+            try {
+                $instancename = $instance.InstanceName
+                $services = Get-DbaService -ComputerName $instance
+                $dbengine = $services | Where-Object DisplayName -match "SQL Server \($instancename\)"
+                $dbaccount = $dbengine.StartName
+                $agentengine = $services | Where-Object DisplayName -match "SQL Server Agent \($instancename\)"
+                $agentaccount = $agentengine.StartName
 
-                    if ($dbaccount.length -lt 2) {
-                        Stop-Function -Message "Couldn't get service information for $instance, moving on" -Continue
-                    }
+                if ($dbaccount.length -lt 2) {
+                    Stop-Function -Message "Couldn't get service information for $instance, moving on" -Continue
+                }
 
-                    foreach ($folder in $Path) {
-                        Write-Message -Level Verbose -Message "Modifying $folder on $instance"
-                        $remote = Join-AdminUnc -Servername $server.ComputerName -FilePath $folder
+                foreach ($folder in $Path) {
+                    Write-Message -Level Verbose -Message "Modifying $folder on $instance"
+                    $remote = Join-AdminUnc -Servername $server.ComputerName -FilePath $folder
 
+                    if ($PSCmdlet.ShouldProcess($instance, "Removing permission protections for $remote")) {
                         try {
                             $acl = Get-Acl -Path $remote
                             $acl.SetAccessRuleProtection($true, $true)
                             $null = Set-Acl -Path $remote -AclObject $acl
                         } catch {
-                            Stop-Function -Message "Issing setting file permissions on $remote" -ErrorRecord $_ -Continue
-
+                            Stop-Function -Message "Issue setting file permissions on $remote" -ErrorRecord $_ -Continue
                         }
+                    }
 
-                        $acl = Get-Acl -Path $remote
-                        $access = $acl.Access
+                    $acl = Get-Acl -Path $remote
+                    $access = $acl.Access
 
-                        foreach ($a in $access) {
+                    foreach ($a in $access) {
+                        $accessrule = "$($a.IdentityReference) - $($a.AccessControlType) - $($a.FileSystemRights)"
+                        if ($PSCmdlet.ShouldProcess($instance, "Removing access rule $accessrule from $remote")) {
                             $null = $acl.RemoveAccessRule($a)
                         }
+                    }
 
-                        # Add local admin
-                        $accountdisplay = @()
-                        foreach ($username in $Account) {
-                            $accountdisplay += $username
+                    # Add local admin
+                    $accountdisplay = @()
+                    foreach ($username in $Account) {
+                        $accountdisplay += $username
+                        if ($PSCmdlet.ShouldProcess($instance, "Adding full control for $username on $remote")) {
                             $permission = $username, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
                             $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
                             $acl.SetAccessRule($rule)
                         }
+                    }
 
+                    if ($PSCmdlet.ShouldProcess($instance, "Setting the full control permissions for $username on $remote")) {
                         $accountdisplay += $dbaccount
                         $permission = "$dbaccount", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
                         $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
@@ -139,21 +152,26 @@ function Set-DbsAcl {
                             $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
                             $acl.SetAccessRule($rule)
                         }
-                        $null = Set-Acl -Path $remote -AclObject $acl
                     }
 
-                    [PSCustomObject]@{
-                        ComputerName        = $server.ComputerName
-                        InstanceName        = $server.ServiceName
-                        SqlInstance         = $server.DomainInstanceName
-                        Account             = $accountdisplay -join ", "
-                        Status              = "Success"
-                        Path                = $Path -join ", "
-                        PreviousPermissions = $access
+                    if ($PSCmdlet.ShouldProcess($instance, "Changing the owner for $remote")) {
+                        $acl2 = Get-Acl -Path $remote
+                        $acl2.SetOwner([System.Security.Principal.NTAccount]$Owner)
+                        $null = Set-Acl -Path $remote -AclObject $acl2
+                        $null = Set-Acl -Path $remote -AclObject $acl
+                        [PSCustomObject]@{
+                            ComputerName        = $server.ComputerName
+                            InstanceName        = $server.ServiceName
+                            SqlInstance         = $server.DomainInstanceName
+                            Account             = $accountdisplay -join ", "
+                            Status              = "Success"
+                            Path                = $Path -join ", "
+                            PreviousPermissions = $access
+                        }
                     }
-                } catch {
-                    Stop-Function -Message "Failed to set permissions on $instance" -ErrorRecord $_ -Continue -Target $instance
                 }
+            } catch {
+                Stop-Function -Message "Failed to set permissions on $instance" -ErrorRecord $_ -Continue -Target $instance
             }
         }
     }
