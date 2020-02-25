909e3e9465f5e41c8fccc6d80d360f2257f72f35
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2020-02-25 12:09:08 +0000

    Add SystemPermission (#46)

---
+++ public/Get-DbsSystemPermission.ps1
@@ -0,0 +1,87 @@
+function Get-DbsSystemPermission {
+    <#
+    .SYNOPSIS
+        Gets permissions that were identified as not allowed in the check content
+
+    .DESCRIPTION
+        Gets permissions that were identified as not allowed in the check content
+
+    .PARAMETER SqlInstance
+        The target SQL Server instance or instances. Server version must be SQL Server version 2012 or higher.
+
+    .PARAMETER SqlCredential
+        Login to the target instance using alternative credentials. Accepts PowerShell credentials (Get-Credential).
+
+        Windows Authentication, SQL Server Authentication, Active Directory - Password, and Active Directory - Integrated are all supported.
+
+        For MFA support, please use Connect-DbaInstance.
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+        Tags: V-79129
+        Author: Chrissy LeMaire (@cl), netnerds.net
+
+        Copyright: (c) 2020 by Chrissy LeMaire, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .EXAMPLE
+        PS C:\> Get-DbsSystemPermission -SqlInstance sql2017, sql2016, sql2012
+
+        Gets a list of non-compliant audit states on sql2017, sql2016 and sql2012
+
+    .EXAMPLE
+        PS C:\> Get-DbsSystemPermission -SqlInstance sql2017, sql2016, sql2012
+
+        Gets a list of non-compliant audit states on sql2017, sql2016 and sql2012
+
+    .EXAMPLE
+        PS C:\> Get-DbsSystemPermission -SqlInstance sql2017, sql2016, sql2012 | Export-Csv -Path D:\DISA\auditstates.csv -NoTypeInformation
+
+        Gets a list of non-compliant audit states sql2017, sql2016 and sql2012 to D:\disa\auditstates.csv
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipeline)]
+        [DbaInstanceParameter[]]$SqlInstance,
+        [PsCredential]$SqlCredential,
+        [switch]$EnableException
+    )
+    process {
+        $servers = Connect-DbaInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential
+        foreach ($server in $servers) {
+            try {
+                $cluster = $server.Query("SELECT SERVERPROPERTY('IsClustered') as IsClustered, SERVERPROPERTY('IsHadrEnabled') as IsHadrEnabled")
+
+                if ($cluster.IsClustered -and $cluster.IsHadrEnabled) {
+                    $collection = "CONNECT SQL", "ALTER ANY AVAILABILITY GROUP", "VIEW SERVER STATE ", "VIEW ANY DATABASE"
+                } elseif ($cluster.IsClustered -and -not $cluster.IsHadrEnabled) {
+                    $collection = "CONNECT SQL", "VIEW SERVER STATE ", "VIEW ANY DATABASE"
+                } elseif (-not $cluster.IsClustered -and $cluster.IsHadrEnabled) {
+                    $collection = "CONNECT SQL", "ALTER ANY AVAILABILITY GROUP", "VIEW ANY DATABASE"
+                } else {
+                    $collection = "CONNECT SQL", "VIEW ANY DATABASE"
+                }
+
+                $results = $server.Query("EXECUTE AS LOGIN = 'NT AUTHORITY\SYSTEM'
+                                SELECT permission_name FROM fn_my_permissions(NULL,NULL)
+                                REVERT")
+
+                $results = $results | Where-Object permission_name -notin $collection
+                foreach ($result in $results.permission_name) {
+                    [pscustomobject]@{
+                        SqlInstance = $server.Name
+                        Login       = "NT AUTHORITY\SYSTEM"
+                        Permission  = $result
+                        Server      = $server
+                    } | Select-DefaultView -Property SqlInstance, Login, Permission
+                }
+            } catch {
+                Stop-Function -Message "Failure for $($server.Name)" -ErrorRecord $_ -Continue -EnableException:$EnableException
+            }
+        }
+    }
+}
