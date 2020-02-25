04edb8a601fd4bfa51b697d5ffdf8784915970f4
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2020-02-25 12:06:49 +0000

    still broken - try changing the owner earlier (#47)
    * still broken - try changing the owner earlier
    
    * fixed!

--- public/Set-DbsAcl.ps1
+++ public/Set-DbsAcl.ps1
@@ -13,12 +13,17 @@ function Set-DbsAcl {
     .PARAMETER SqlInstance
         The target SQL Server instance or instances.
 
+        This is required to get specific information about the paths to modify. The base computer name is also used to
+        perform the actual modifications.
+
     .PARAMETER SqlCredential
-        Login to the target instance using alternative credentials. Accepts PowerShell credentials (Get-Credential).
+        Login to the target _SQL Server_ instance using alternative credentials. Accepts PowerShell credentials (Get-Credential).
 
-        Windows Authentication, SQL Server Authentication, Active Directory - Password, and Active Directory - Integrated are all supported.
+    .PARAMETER Credential
+        Login to the target _Windows_ server using alternative credentials. Accepts PowerShell credentials (Get-Credential).
 
-        For MFA support, please use Connect-DbaInstance.
+    .PARAMETER Owner
+        The account that will be set as the folder owner.
 
     .PARAMETER Account
         The account name or names that are to be granted permissions along with the service accounts.
@@ -51,14 +56,16 @@ function Set-DbsAcl {
         License: MIT https://opensource.org/licenses/MIT
 
     .EXAMPLE
-        PS C:\> Set-DbsAcl -SqlInstance sql2017, sql2016, sql2012 -Account "AD\SQL Admins"
+        PS C:\> Set-DbsAcl -SqlInstance sql2017, sql2016, sql2012 -Account "AD\SQL Admins" -Owner "AD\SQL Service"
 
         Sets permissions for the default data, log and backups on sql2017, sql2016, sql2012.
 
-        Adds appropriate permissions for the "AD\SQL Admins" group as well as the SQL Server service accounts.
+        Adds appropriate permissions for the "AD\SQL Admins" group as well as the SQL Server service accountsas Full Access.
+
+        Also sets the owner of the folder to "AD\SQL Service"
 
     .EXAMPLE
-        PS C:\> Get-DbaRegServer -SqlInstance sqlcentral | Set-DbsAcl -Account "AD\SQL Admins"
+        PS C:\> Get-DbaRegServer -SqlInstance sqlcentral | Set-DbsAcl -Account "AD\SQL Admins" -Owner "AD\SQL Service"
 
         Sets the appropriate permissions for all SQL Servers stored in the sqlcentral registered server.
     #>
@@ -67,18 +74,20 @@ function Set-DbsAcl {
         [parameter(Mandatory, ValueFromPipeline)]
         [DbaInstanceParameter[]]$SqlInstance,
         [PsCredential]$SqlCredential,
+        [PsCredential]$Credential,
+        [parameter(Mandatory)]
+        [string]$Owner,
         [parameter(Mandatory)]
         [string[]]$Account,
         [string[]]$Path,
         [switch]$EnableException
     )
-
     process {
         foreach ($instance in $SqlInstance) {
             try {
                 $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential
             } catch {
-                Stop-Function -Message "Error occurred while establishing connection to $instance" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+                Stop-PSFFunction -Message "Error occurred while establishing connection to $instance" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
             }
 
             if (-not $PSBoundParameters.Path) {
@@ -86,74 +95,142 @@ function Set-DbsAcl {
                 $Path = $defaults.Data, $defaults.Log, $defaults.Backup | Where-Object { $_ -notmatch '\\\\' } | Select-Object -Unique
             }
 
-            if ($PSCmdlet.ShouldProcess($instance, "Changing permissions for $Path for $instance")) {
-                try {
-                    $instancename = $instance.InstanceName
-                    $services = Get-DbaService -ComputerName $instance
-                    $dbengine = $services | Where-Object DisplayName -match "SQL Server \($instancename\)"
-                    $dbaccount = $dbengine.StartName
-                    $agentengine = $services | Where-Object DisplayName -match "SQL Server Agent \($instancename\)"
-                    $agentaccount = $agentengine.StartName
+            try {
+                $computername = $instance.ComputerName
+                $instancename = $instance.InstanceName
+                $services = Get-DbaService -ComputerName $instance -Credential $Credential 3>$null
+                $dbengine = $services | Where-Object DisplayName -match "SQL Server \($instancename\)"
+                $dbaccount = $dbengine.StartName
+                $agentengine = $services | Where-Object DisplayName -match "SQL Server Agent \($instancename\)"
+                $agentaccount = $agentengine.StartName
 
-                    if ($dbaccount.length -lt 2) {
-                        Stop-Function -Message "Couldn't get service information for $instance, moving on" -Continue
+                if ($dbaccount.length -lt 2) {
+                    Stop-PSFFunction -Message "Couldn't get service information for $instance, moving on" -Continue
+                }
+
+                foreach ($folder in $Path) {
+                    Write-Message -Level Verbose -Message "Modifying $folder on $computername"
+
+                    if ($PSCmdlet.ShouldProcess($computername, "Removing permission protections for $folder")) {
+                        try {
+                            Invoke-PSFCommand -ComputerName $computername -Credential $credential -ScriptBlock {
+                                param ($folder)
+                                # set it as a script variable to ensure it persists in the session, may be excessive
+                                $script:acl = Get-Acl -Path $folder -ErrorAction Stop
+                                $script:acl.SetAccessRuleProtection($true, $true)
+                                $null = Set-Acl -Path $folder -AclObject $script:acl -ErrorAction Stop
+                            } -ArgumentList $folder -ErrorAction Stop
+                        } catch {
+                            Stop-PSFFunction -Message "Issue setting file permissions on $folder" -ErrorRecord $_ -Continue
+                        }
                     }
 
-                    foreach ($folder in $Path) {
-                        Write-Message -Level Verbose -Message "Modifying $folder on $instance"
-                        $remote = Join-AdminUnc -Servername $server.ComputerName -FilePath $folder
-
+                    if ($PSCmdlet.ShouldProcess($computername, "Collecting all access rules for $folder")) {
                         try {
-                            $acl = Get-Acl -Path $remote
-                            $acl.SetAccessRuleProtection($true, $true)
-                            $null = Set-Acl -Path $remote -AclObject $acl
+                            $access = Invoke-PSFCommand -ComputerName $computername -Credential $credential -ScriptBlock {
+                                param ($folder)
+                                (Get-Acl -Path $folder -ErrorAction Stop).Access
+                            } -ArgumentList $folder -ErrorAction Stop
                         } catch {
-                            Stop-Function -Message "Issing setting file permissions on $remote" -ErrorRecord $_ -Continue
-
+                            Stop-PSFFunction -Message "Issue collecting file permissions on $folder" -ErrorRecord $_ -Continue
                         }
+                    }
 
-                        $acl = Get-Acl -Path $remote
-                        $access = $acl.Access
+                    if ($PSCmdlet.ShouldProcess($computername, "Removing all access rules for $folder")) {
+                        try {
+                            Invoke-PSFCommand -ComputerName $computername -Credential $credential -ScriptBlock {
+                                param ($folder, $VerbosePreference)
+                                $script:acl = Get-Acl -Path $folder
+                                $access = $script:acl.Access
 
-                        foreach ($a in $access) {
-                            $null = $acl.RemoveAccessRule($a)
+                                foreach ($a in $access) {
+                                    $accessrule = "$($a.IdentityReference) - $($a.AccessControlType) - $($a.FileSystemRights)"
+                                    Write-Verbose -Message "Removing access rule $accessrule from $folder on $env:COMPUTERNAME"
+                                    $null = $script:acl.RemoveAccessRule($a)
+                                }
+                            } -ArgumentList $folder, $VerbosePreference -ErrorAction Stop
+                        } catch {
+                            Stop-PSFFunction -Message "Issue setting file permissions on $folder" -ErrorRecord $_ -Continue
                         }
+                    }
 
-                        # Add local admin
-                        $accountdisplay = @()
-                        foreach ($username in $Account) {
-                            $accountdisplay += $username
-                            $permission = $username, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
-                            $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
-                            $acl.SetAccessRule($rule)
+                    # Add local admin
+                    $accountdisplay = @()
+                    foreach ($username in $Account) {
+                        $accountdisplay += $username
+                        if ($PSCmdlet.ShouldProcess($computername, "Adding full control for $username on $folder")) {
+                            try {
+                                Invoke-PSFCommand -ComputerName $computername -Credential $credential -ScriptBlock {
+                                    param ($username, $VerbosePreference)
+                                    $permission = $username, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
+                                    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+                                    $script:acl.SetAccessRule($rule)
+                                } -ArgumentList $username, $VerbosePreference -ErrorAction Stop
+                            } catch {
+                                Stop-PSFFunction -Message "Issue setting file permissions for $username on $folder" -ErrorRecord $_ -Continue
+                            }
                         }
+                    }
 
+                    if ($PSCmdlet.ShouldProcess($computername, "Setting the full control permissions for $dbaccount on $folder")) {
                         $accountdisplay += $dbaccount
-                        $permission = "$dbaccount", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
-                        $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
-                        $acl.SetAccessRule($rule)
-
                         if ($dbaccount -ne $agentaccount) {
                             $accountdisplay += $agentaccount
-                            $permission = "$agentaccount", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
-                            $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
-                            $acl.SetAccessRule($rule)
                         }
-                        $null = Set-Acl -Path $remote -AclObject $acl
+                        try {
+                            $null = Invoke-PSFCommand -ComputerName $computername -Credential $credential -ScriptBlock {
+                                param ($dbaccount, $agentaccount, $VerbosePreference)
+                                $permission = "$dbaccount", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
+                                $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+                                $script:acl.SetAccessRule($rule)
+
+                                if ($dbaccount -ne $agentaccount) {
+                                    $accountdisplay += $agentaccount
+                                    $permission = "$agentaccount", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
+                                    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+                                    $script:acl.SetAccessRule($rule)
+                                }
+                            } -ArgumentList $dbaccount, $agentaccount, $VerbosePreference -ErrorAction Stop
+                        } catch {
+                            Stop-PSFFunction -Message "Issue setting file permissions on $folder for $dbaacount or $agentaccount" -ErrorRecord $_ -Continue
+                        }
                     }
 
-                    [PSCustomObject]@{
-                        ComputerName        = $server.ComputerName
-                        InstanceName        = $server.ServiceName
-                        SqlInstance         = $server.DomainInstanceName
-                        Account             = $accountdisplay -join ", "
-                        Status              = "Success"
-                        Path                = $Path -join ", "
-                        PreviousPermissions = $access
+                    if ($PSCmdlet.ShouldProcess($computername, "Changing the owner for $folder")) {
+                        try {
+                            $null = Invoke-PSFCommand -ComputerName $computername -Credential $credential -ScriptBlock {
+                                param ($Owner)
+                                $script:acl.SetOwner([System.Security.Principal.NTAccount]$Owner)
+                            } -ArgumentList $Owner -ErrorAction Stop
+                        } catch {
+                            Stop-PSFFunction -Message "Changing owner on $folder on $computername" -ErrorRecord $_ -Continue
+                        }
                     }
-                } catch {
-                    Stop-Function -Message "Failed to set permissions on $instance" -ErrorRecord $_ -Continue -Target $instance
+
+                    if ($PSCmdlet.ShouldProcess($computername, "Performing the actual set")) {
+                        try {
+                            $null = Invoke-PSFCommand -ComputerName $computername -Credential $credential -ScriptBlock {
+                                param ($folder)
+                                $null = Set-Acl -Path $folder -AclObject $script:acl
+                            } -ArgumentList $folder -ErrorAction Stop
+                        } catch {
+                            Stop-PSFFunction -Message "Changing owner on $folder on $computername" -ErrorRecord $_ -Continue
+                        }
+
+                        [PSCustomObject]@{
+                            ComputerName        = $server.ComputerName
+                            InstanceName        = $server.ServiceName
+                            SqlInstance         = $server.DomainInstanceName
+                            Path                = $folder
+                            Owner               = $Owner
+                            Account             = $accountdisplay -join ", "
+                            PreviousPermissions = $access
+                            Status              = "Success"
+                        }
+                    }
                 }
+            } catch {
+                Stop-PSFFunction -Message "Failed to set permissions on $instance" -ErrorRecord $_ -Continue -Target $instance
             }
         }
     }
