bf7223b780bab0d7a9e7bcc39eb8bfb31895b6f0
Chrissy LeMaire <clemaire@gmail.com>
2020-02-26 02:44:27 +0100

    add tag

--- public/Get-DbsDbAlterPermission.ps1
+++ public/Get-DbsDbAlterPermission.ps1
@@ -25,7 +25,7 @@ function Get-DbsDbAlterPermission {
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
     .NOTES
-        Tags: V-79109
+        Tags: V-79109, V-79075
         Author: Chrissy LeMaire (@cl), netnerds.net
 
         Copyright: (c) 2020 by Chrissy LeMaire, licensed under MIT
