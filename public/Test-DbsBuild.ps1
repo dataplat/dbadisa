67b4c27909824f440c34505567e316428c652f93
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2020-02-28 09:11:07 +0000

    V-79249 - Test-DbsBuild (#72)

---
+++ public/Test-DbsBuild.ps1
@@ -0,0 +1,65 @@
+function Test-DbsBuild {
+    <#
+    .SYNOPSIS
+        Obtains evidence that software patches are consistently applied to SQL Server within the time frame defined for each patch.
+
+    .DESCRIPTION
+        Obtains evidence that software patches are consistently applied to SQL Server within the time frame defined for each patch.
+
+    .PARAMETER SqlInstance
+        The target SQL Server instance or instances. Server version must be SQL Server version 2012 or higher.
+
+    .PARAMETER SqlCredential
+        Login to the target instance using alternative credentials. Accepts PowerShell credentials (Get-Credential).
+
+        Windows Authentication, SQL Server Authentication, Active Directory - Password, and Active Directory - Integrated are all supported.
+
+        For MFA support, please use Connect-DbaInstance.
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+        Tags: V-79249
+        Author: Chrissy LeMaire (@cl), netnerds.net
+
+        Copyright: (c) 2020 by Chrissy LeMaire, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .EXAMPLE
+        PS C:\> Test-DbsBuild -SqlInstance sql2017, sql2016, sql2012
+
+        Obtains evidence that software patches are consistently applied to SQL Server within the time frame defined for each patch.
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipeline)]
+        [DbaInstanceParameter[]]$SqlInstance,
+        [PsCredential]$SqlCredential,
+        [switch]$EnableException
+    )
+    process {
+        foreach ($instance in $SqlInstance) {
+            try {
+                $server = Connect-DbaInstance -SqlInstance $instance -SqlCredential $SqlCredential -DisableException:$(-not $EnableException)
+                $build = Get-DbaBuildReference -SqlInstance $server
+                $outdated = $build | Where-Object SupportedUntil -lt (Get-Date)
+                $latest = Test-DbaBuild -SqlInstance $server -Latest
+
+                if ($outdated -or -not $latest.Compliant) {
+                    [PSCustomObject]@{
+                        SqlInstance    = $server.Name
+                        SupportedUntil = $build.SupportedUntil
+                        BuildLevel     = $latest.BuildLevel
+                        BuildTarget    = $latest.BuildTarget
+                        Compliant      = $false
+                    }
+                }
+            } catch {
+                Stop-Function -Message "Failure for $($server.Name)" -ErrorRecord $_ -Continue -EnableException:$EnableException
+            }
+        }
+    }
+}
