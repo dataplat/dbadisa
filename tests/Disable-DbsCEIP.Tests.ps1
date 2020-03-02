0f02283648d23b0b8196d613ffe66835d74faf7e
Chrissy LeMaire <clemaire@gmail.com>
2020-03-02 23:56:27 +0100

    Add test for Disable-DbsCEIP

---
+++ tests/Disable-DbsCEIP.Tests.ps1
@@ -0,0 +1,18 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+    Context "Disables SQL Server Browser on localhost" {
+        $results = Disable-DbsCEIP -ComputerName $env:COMPUTERNAME
+        It "should report that CEIP was disabled" {
+            $results.Enabled | Should -Be $false
+        }
+        $services = Get-Service *SQLTELEMETRY*  -ComputerName $env:COMPUTERNAME
+        foreach ($service in $services) {
+            It "should actually be disabled" {
+                $service.StartType | Should -Be "Disabled"
+            }
+        }
+    }
+}
