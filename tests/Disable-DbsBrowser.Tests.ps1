96bea10dec8e1d7c582f194e1eac1fcc8ef0ad6f
Chrissy LeMaire <clemaire@gmail.com>
2020-03-03 00:09:56 +0100

    a lil cleanup

--- tests/Disable-DbsBrowser.Tests.ps1
+++ tests/Disable-DbsBrowser.Tests.ps1
@@ -1,7 +1,4 @@
 $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
-Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
-. "$PSScriptRoot\constants.ps1"
-
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     Context "Disables SQL Server Browser on localhost" {
         $results = Disable-DbsBrowser -ComputerName $env:COMPUTERNAME
