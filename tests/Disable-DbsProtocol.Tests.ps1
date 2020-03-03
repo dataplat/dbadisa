661a3918911364a76d384f82a56747dc8036f14a
Chrissy LeMaire <clemaire@gmail.com>
2020-03-03 21:00:54 +0100

    Add Disable-DbsProtocol

---
+++ tests/Disable-DbsProtocol.Tests.ps1
@@ -0,0 +1,12 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+    Context "Disables some protocols on localhost" {
+        It "should report that a protocol is disabled" {
+            $results = Disable-DbsProtocol -ComputerName $env:COMPUTERNAME
+            $results.Disabled | Should -Contain $true
+        }
+        It "should really be disabled" {
+            Get-DbaInstanceProtocol -ComputerName $env:COMPUTERNAME | Where-Object Name -ne Tcp | Select-Object IsEnabled | Should -Not -Contain $true
+        }
+    }
+}
