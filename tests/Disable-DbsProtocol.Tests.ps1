60c8bbf7cf57554c6830abbbce6b69e0d407b1d5
Chrissy LeMaire <clemaire@gmail.com>
2020-03-03 21:03:00 +0100

    add whatif test

--- tests/Disable-DbsProtocol.Tests.ps1
+++ tests/Disable-DbsProtocol.Tests.ps1
@@ -8,5 +8,9 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
         It "should really be disabled" {
             Get-DbaInstanceProtocol -ComputerName $env:COMPUTERNAME | Where-Object Name -ne Tcp | Select-Object IsEnabled | Should -Not -Contain $true
         }
+        $results = Disable-DbsProtocol -SqlInstance $env:COMPUTERNAME -WhatIf
+        It "should not return any objects when using whatif" {
+            $results | Should -Be $null
+        }
     }
 }
