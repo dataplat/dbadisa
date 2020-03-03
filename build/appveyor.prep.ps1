744bb95eb1db3d38f2a675154fa29bcf3a49cf15
Chrissy LeMaire <clemaire@gmail.com>
2020-03-03 09:33:36 +0100

    skip force

--- build/appveyor.prep.ps1
+++ build/appveyor.prep.ps1
@@ -3,7 +3,7 @@ $sw = [system.diagnostics.stopwatch]::startNew()
 
 #Get PSScriptAnalyzer (to check warnings)
 Write-Host -Object "appveyor.prep: Install PSScriptAnalyzer" -ForegroundColor DarkGreen
-Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck | Out-Null
+Install-Module -Name PSScriptAnalyzer -SkipPublisherCheck | Out-Null
 
 #Get Pester (to run tests)
 Write-Host -Object "appveyor.prep: Install Pester" -ForegroundColor DarkGreen
@@ -11,15 +11,15 @@ choco install pester | Out-Null
 
 #Get PSFramework (dependency)
 Write-Host -Object "appveyor.prep: Install PSFramework" -ForegroundColor DarkGreen
-Install-Module -Name PSFramework -Force -SkipPublisherCheck | Out-Null
+Install-Module -Name PSFramework -SkipPublisherCheck | Out-Null
 
 #Get dbatools (dependency)
 Write-Host -Object "appveyor.prep: Install dbatools" -ForegroundColor DarkGreen
-Install-Module -Name dbatools -Force -SkipPublisherCheck | Out-Null
+Install-Module -Name dbatools -SkipPublisherCheck | Out-Null
 
 #Get dbachecks (dependency)
 Write-Host -Object "appveyor.prep: Install dbachecks" -ForegroundColor DarkGreen
-Install-Module -Name dbachecks -Force -SkipPublisherCheck | Out-Null
+Install-Module -Name dbachecks -SkipPublisherCheck | Out-Null
 
 $null = mkdir C:\temp
 
