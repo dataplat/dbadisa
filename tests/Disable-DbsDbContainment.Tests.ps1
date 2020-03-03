e5cfa320d7973d9525912aed41cb0e6f736558c7
Chrissy LeMaire <clemaire@gmail.com>
2020-03-03 12:32:23 +0100

    disable first

--- tests/Disable-DbsDbContainment.Tests.ps1
+++ tests/Disable-DbsDbContainment.Tests.ps1
@@ -1,5 +1,8 @@
 $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+    BeforeAll {
+        $null = Set-DbaSpConfigure -SqlInstance $env:COMPUTERNAME -Name ContainmentEnabled -Value 1
+    }
     Context "Disables database containment on localhost" {
         It "should report that containment is disabled" {
             $results = Disable-DbsDbContainment -SqlInstance $env:COMPUTERNAME
