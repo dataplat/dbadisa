99c6cecd72c5b1a15e3e040fd565a6f97485fcec
Chrissy LeMaire <clemaire@gmail.com>
2020-03-03 12:58:12 +0100

    Disable-DbsExternalScripts test

---
+++ tests/Disable-DbsExternalScripts.Tests.ps1
@@ -0,0 +1,17 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+    BeforeAll {
+        $null = Set-DbaSpConfigure -SqlInstance $env:COMPUTERNAME -Name ExternalScriptsEnabled -Value 1
+    }
+    Context "Disables external scripts on localhost" {
+        It "should report that polybase is disabled" {
+            $results = Disable-DbsExternalScripts -SqlInstance $env:COMPUTERNAME
+            $results.NewValue | Should -Be 0
+        }
+
+        It "should actually be disabled" {
+            $config = Get-DbaSpConfigure -SqlInstance $env:COMPUTERNAME -Name ExternalScriptsEnabled
+            $config.RunningValue | Should -Be 0
+        }
+    }
+}
