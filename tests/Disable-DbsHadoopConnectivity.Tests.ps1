dbd9919dd52769ddd87c7b835917212d8364940d
Chrissy LeMaire <clemaire@gmail.com>
2020-03-03 13:07:52 +0100

    HadoopConnectivity

---
+++ tests/Disable-DbsHadoopConnectivity.Tests.ps1
@@ -0,0 +1,17 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+    BeforeAll {
+        $null = Set-DbaSpConfigure -SqlInstance $env:COMPUTERNAME -Name HadoopConnectivity -Value 1
+    }
+    Context "Disables polybase on localhost" {
+        It "should report that polybase is disabled" {
+            $results = Disable-DbsHadoopConnectivity -SqlInstance $env:COMPUTERNAME
+            $results.NewValue | Should -Be 0
+        }
+
+        It "should actually be disabled" {
+            $config = Get-DbaSpConfigure -SqlInstance $env:COMPUTERNAME -Name HadoopConnectivity
+            $config.RunningValue | Should -Be 0
+        }
+    }
+}
