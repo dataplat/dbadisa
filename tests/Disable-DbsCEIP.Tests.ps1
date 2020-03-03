262792a0bbe2a9f5ccf57e12027a3ba595df244b
Chrissy LeMaire <clemaire@gmail.com>
2020-03-03 12:29:10 +0100

    Disable-DbsCEIP

--- tests/Disable-DbsCEIP.Tests.ps1
+++ tests/Disable-DbsCEIP.Tests.ps1
@@ -1,11 +1,12 @@
 $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     Context "Disables SQL Server Browser on localhost" {
-        $results = Disable-DbsCEIP -ComputerName $env:COMPUTERNAME
         It "should report that CEIP was disabled" {
+            $results = Disable-DbsCEIP -ComputerName $env:COMPUTERNAME
             $results.Enabled | Should -Be $false
         }
-        $services = Get-Service *SQLTELEMETRY*  -ComputerName $env:COMPUTERNAME
+
+        $services = Get-Service *SQLTELEMETRY* -ComputerName $env:COMPUTERNAME
         foreach ($service in $services) {
             It "$($service.Name) should actually be disabled" {
                 $service.StartType | Should -Be "Disabled"
